0d52c0265ff459a76cef6e540a0dcfdd88dad9c7
Vagner H <vh3rd7@gmail.com>
2023-12-02 21:27:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TypeScript Jest
+
+- https://jestjs.io/docs/getting-started
+- https://www.npmjs.com/package/ts-jest
+
+```
+yarn add -D jest typescript
+yarn add -D ts-node-dev
+yarn add -D ts-jest @types/jest
+```
---
+++ index.spec.ts
@@ -0,0 +1,23 @@
+import { helloWorld } from './index';
+
+it("should be true", () => {
+  expect(true).toBeTruthy();
+});
+
+describe("test group", () => {
+
+  it("should return false", () => {
+    const result = () => (true);
+    expect(result()).toBeFalsy();
+  });
+
+  it("should the helloWorld function return a string 'Hello World!'", () => {
+    // Arrange
+    const expected = "Hello World!";
+    // Act
+    const received = helloWorld();
+    // Assert
+    expect(typeof received).toBe("string");
+    expect(received).toEqual(expected);
+  });
+});
